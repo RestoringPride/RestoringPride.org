e533784b49b14dd12e438c59535bb6a1ce39dcb5
David G. Moore, Jr <dev@dgmjr.io>
2023-03-15 10:53:52 -0400

    Update startup.sh

--- files/httpdocs/startup.sh
+++ files/httpdocs/startup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-type -p curl 2>/dev/null || sudo apt install curl -y
-sudo curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
 gh auth login --with-token $GITHUB_TOKEN
 git clone https://github.com/RestoringPride/RestoringPride.org /home/RestoringPride.org
 cp RestoringPride.org/wp-content/themes/** /home/site/wwwroot/wp-content/themes/
